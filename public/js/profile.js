7c38c32aaefa518fc41e1febea824863328f9eaf
Irina <irkag22@gmail.com>
2023-03-11 20:14:58 -0700

    mesed around w profile.js

--- public/js/profile.js
+++ public/js/profile.js
@@ -112,49 +112,133 @@
 // document.querySelector('.recipe-list').addEventListener('click', delButtonHandler);
 
 
+// const newFormHandler = async (event) => {
+//   event.preventDefault();
+
+//   const title = document.querySelector('#search-input').value.trim();
+
+//   if (title) {
+//     const response = await fetch(`/api/recipe`, {
+//       method: 'POST',
+//       body: JSON.stringify({ title }),
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//     });
+
+//     if (response.ok) {
+//       console.log()
+//       // document.location.replace('/profile');
+//     } else {
+//       alert('Failed to create project');
+//     }
+//   }
+// };
+
+// // // const delButtonHandler = async (event) => {
+// // //   if (event.target.hasAttribute('data-id')) {
+// // //     const id = event.target.getAttribute('data-id');
+
+// // //     const response = await fetch(`/api/projects/${id}`, {
+// // //       method: 'DELETE',
+// // //     });
+
+// // //     if (response.ok) {
+// // //       document.location.replace('/profile');
+// // //     } else {
+// // //       alert('Failed to delete project');
+// // //     }
+// // //   }
+// // };
+
+// document
+//   .querySelector('.new-project-form')
+//   .addEventListener('submit', newFormHandler);
+
+// // document
+// //   .querySelector('.project-list')
+// //   .addEventListener('click', delButtonHandler);
+
+
+// router.delete('/recipe/:id', async (req, res) => {
+//   try {
+//     const deletedRecipe = await Recipe.destroy({
+//       where: {
+//         id: req.params.id,
+//       },
+//     });
+//     res.status(200).json(deletedRecipe);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(router);
+
 const newFormHandler = async (event) => {
   event.preventDefault();
+  const Title = document.querySelector('#recipe-name').value.trim();
+  const Ingredients = document.querySelector('#recipe-ingredients').value.trim();
+  const Instructions = document.querySelector('#recipe-instructions').value.trim();
+  const Image_Name = document.querySelector('#recipe-image').value.trim();
+  const Cleaned_Ingredients = document.querySelector('#recipe-cleaned-ingredients').value.trim();
 
-  const title = document.querySelector('#search-input').value.trim();
-
-  if (title) {
-    const response = await fetch(`/api/recipe`, {
+  if (Title) {
+    const response = await fetch('./seeds/recipeData.json', {
       method: 'POST',
-      body: JSON.stringify({ title }),
+      body: JSON.stringify({ Title, Ingredients, Instructions, Image_Name, Cleaned_Ingredients }),
       headers: {
         'Content-Type': 'application/json',
       },
     });
 
     if (response.ok) {
-      console.log()
-      // document.location.replace('/profile');
+      document.location.replace('/profile');
     } else {
-      alert('Failed to create project');
+      alert('Failed to create recipe');
     }
   }
 };
 
-// // const delButtonHandler = async (event) => {
-// //   if (event.target.hasAttribute('data-id')) {
-// //     const id = event.target.getAttribute('data-id');
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
 
-// //     const response = await fetch(`/api/projects/${id}`, {
-// //       method: 'DELETE',
-// //     });
+    const response = await fetch(`/api/recipe/${Title}`, {
+      method: 'DELETE',
+    });
 
-// //     if (response.ok) {
-// //       document.location.replace('/profile');
-// //     } else {
-// //       alert('Failed to delete project');
-// //     }
-// //   }
-// };
+    if (response.ok) {
+      window.location.reload();
+    } else {
+      alert('Failed to delete recipe');
+    }
+  }
+};
 
-document
-  .querySelector('.new-project-form')
-  .addEventListener('submit', newFormHandler);
 
-// document
-//   .querySelector('.project-list')
-//   .addEventListener('click', delButtonHandler);
+
+const searchForm = document.querySelector('#search-form');
+
+searchForm.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const searchValue = document.querySelector('#search-input').value.trim();
+  const response = await fetch(`/api/recipe?title=${searchValue}`);
+  console.log(response);
+
+  if (response.ok) {
+    const recipe = await response.json();
+    // handle data, such as updating HTML with the recipe details
+    console.log(recipe);
+  } else {
+    alert('Failed to get recipe');
+  }
+});
+
+
+
+
+// document.querySelector('.recipe-list').addEventListener('click', delButtonHandler);
