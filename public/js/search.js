e4bf854b8a20926fba5e8041a4ea1334ef40a348
Dschnepf7 <domschnepf1@gmail.com>
2023-03-13 11:35:32 -0500

    added save recipe back end route

--- public/js/search.js
+++ public/js/search.js
@@ -80,29 +80,5 @@ searchForm.addEventListener('submit', async (event) => {
 });
 
 
-
-
-
-// document.querySelector('.recipe-list').addEventListener('click', delButtonHandler);
-
-
-
-
-router.get('/savedRecipes', async (req, res) => {
-  try {
-    // Find the current user
-    const user = await User.findOne({ where: { id: req.session.user_id } });
-
-    // Get all the user's saved recipes
-    const savedRecipes = await user.getRecipes();
-
-    // Render the savedRecipes view with the user's saved recipes
-    res.render('savedRecipes', { savedRecipes });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
-
 module.exports = router;
 
