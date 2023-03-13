ded1be527382546226b5c09470e5769e4859212a
Irina <irkag22@gmail.com>
2023-03-13 12:06:30 -0600

    search.js updated

--- public/js/search.js
+++ public/js/search.js
@@ -1,5 +1,4 @@
 
-
 const newFormHandler = async (event) => {
   event.preventDefault();
   const Title = document.querySelector('#recipe-name').value.trim();
@@ -69,8 +68,9 @@ searchForm.addEventListener('submit', async (event) => {
 
   if (response.ok) {
     const recipeData = await response.json();
-    console.log(recipeData);
+    // console.log(recipeData);
     document.getElementById("recipe-title").textContent=recipeData.recipe.Title;
+    document.getElementById("recipe-title").setAttribute("data-id", recipeData.recipe.id);
     document.getElementById("food-image").src= "/images/Food-Images/"+recipeData.recipe.Image_Name+".jpg"
     document.getElementById("recipe-link").href="/recipe/"+recipeData.recipe.id
     // handle data, such as updating HTML with the recipe details
@@ -80,73 +80,4 @@ searchForm.addEventListener('submit', async (event) => {
 });
 
 
-
-<<<<<<< HEAD
-
-
-// document.querySelector('.recipe-list').addEventListener('click', delButtonHandler);
-
-
-
-
-router.get('/savedRecipes', async (req, res) => {
-  try {
-    // Find the current user
-    const user = await User.findOne({ where: { id: req.session.user_id } });
-
-    // Get all the user's saved recipes
-    const savedRecipes = await user.getRecipes();
-
-    // Render the savedRecipes view with the user's saved recipes
-    res.render('savedRecipes', { savedRecipes });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
-const searchInput = document.querySelector('#search-input');
-const autocompleteDropdown = document.querySelector('#autocomplete-dropdown');
-
-searchInput.addEventListener('input', async () => {
-  const searchValue = searchInput.value.trim();
-
-  if (searchValue) {
-    const response = await fetch(`/api/autocomplete/${searchValue}`);
-
-    if (response.ok) {
-      const suggestions = await response.json();
-
-      // Display the suggestions in the autocomplete menu
-      autocompleteDropdown.innerHTML = '';
-      suggestions.forEach((suggestion) => {
-        const suggestionLink = document.createElement('a');
-        suggestionLink.href = '#';
-        suggestionLink.textContent = suggestion;
-        autocompleteDropdown.appendChild(suggestionLink);
-      });
-      autocompleteDropdown.style.display = 'block';
-
-      // Add event listener for the suggestion links
-      autocompleteDropdown.addEventListener('click', (event) => {
-        event.preventDefault();
-        if (event.target.tagName === 'A') {
-          searchInput.value = event.target.textContent;
-          autocompleteDropdown.innerHTML = '';
-          autocompleteDropdown.style.display = 'none';
-        }
-      });
-    } else {
-      console.error(`Failed to get autocomplete suggestions for "${searchValue}"`);
-    }
-  } else {
-    autocompleteDropdown.innerHTML = '';
-    autocompleteDropdown.style.display = 'none';
-  }
-});
-
-
-
-=======
->>>>>>> main
 module.exports = router;
-
